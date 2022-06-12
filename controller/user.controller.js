886d9d10f43e3752e34915e66ccce9fab0df954c
Shahzad <mech1090@gmail.com>
2022-06-12 15:19:00 -0600

    done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,13 +1,43 @@
-
+const {validationSchema} = require('../validator/user.validator')
+const bcrypt = require('bcrypt')
+const serviceUser = require('../service/user.service')
+const config = require('config')
 
 
 const getLoginFrom = (req,res)=>{
     return res.render('login/layout')
 }
-const login = (req,res)=>{}
+const login = async(req,res)=>{
+    const {email,password} = req.body
+    const fields = {email,password}
+    const findUser = await serviceUser.findEmail({email})
+    if(!findUser){
+        return res.render('signup/layout',{message:'User does not exist Sign up'})
+    }
+    const matchPassword = await bcrypt.compare(password,findUser.password)
+    if(!matchPassword){
+        return res.render('login/layout',{message:'Credentials Wrong'})
+    }
+    return res.render('user/layout')
+
+}
 const getSignupForm = (req,res)=>{
     return res.render('signup/layout')
 }
-const signup = (req,res)=>{}
+const signup = async(req,res)=>{
+    const {email,password} = req.body
+    const field = {email,password}
+    const {error,value} = validationSchema(field)
+    if(error){
+        res.render('signup/layout',{message:error.details[0].message})
+    }
+    const hashPassword = await bcrypt.hash(password,config.get('hash.salt'))
+    const findUser = await serviceUser.findEmail({email})
+    if(findUser){
+       return res.render('login/layout',{message:'User already exists login'})
+    }
+    const createEntry = await serviceUser.createUser({email,password:hashPassword})
+    return res.render('signup/layout',{message:'User Created'})
+}
 
 module.exports = {getLoginFrom,login,getSignupForm,signup}
--- route/user.route.js
+++ route/user.route.js
@@ -5,6 +5,6 @@ const userController = require('../controller/user.controller')
 router.get('/login',userController.getLoginFrom)
 router.post('/login',userController.login)
 router.get('/signup',userController.getSignupForm)
-router.get('/signup',userController.signup)
+router.post('/signup',userController.signup)
 
 module.exports = router
